487c8ba052b1024fee84307cfa5729d4b481ad47
SpartanHarshad <harshadpawar8555@gmail.com>
2020-12-30 17:53:40 +0530

    push phone no button And goto Add Phone No

---
+++ woo/app/src/main/java/com/example/woo/AddName.java
@@ -0,0 +1,14 @@
+package com.example.woo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_name);
+    }
+}
--- woo/app/src/main/java/com/example/woo/MainActivity.java
+++ woo/app/src/main/java/com/example/woo/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.woo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtnSignin ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnSignin = findViewById(R.id.btnsignin);
+        mBtnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EnterMobileNo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
