e49d1f9894456fd7d84c6e313cc0ac57d7059989
Samiran Sardar <samiransardar190@gmail.com>
2020-12-30 20:25:24 +0530

    woo main page

---
+++ woo/app/src/main/java/com/example/woo/MainChooseInterests.java
@@ -0,0 +1,14 @@
+package com.example.woo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainChooseInterests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_choose_interests);
+    }
+}
