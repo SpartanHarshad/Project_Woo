b72fe73c743a1ab8691dbee4cdd86f8658bdee41
SpartanHarshad <harshadpawar8555@gmail.com>
2020-12-31 18:01:16 +0530

    Add Picture And Relationship Lifestyle Activities

---
+++ woo/app/src/main/java/com/example/woo/RelationShip_LiftStyle.java
@@ -0,0 +1,14 @@
+package com.example.woo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelationShip_LiftStyle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relation_ship__lift_style);
+    }
+}
